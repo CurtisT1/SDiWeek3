1dcc6bc68b475f3bf16667794b95a8fa5dbf55b6
CurtisT1 <curtist2fullsail.edu>
2014-06-19 18:57:23 -0500

    Update

--- SDI Project3/rename-me.js
+++ SDI Project3/rename-me.js
@@ -13,13 +13,13 @@ var weapon2 = "Staff";
 var move1 = "Meteor showers";
 
 
-// Outputs of 
+// Outputs of variables
 	
 	
-console.log("The, wizard arrives at the ice witches hideout");
-console.log("The" ,character2, " conjures the biggest ice ball at him. The" ,character1, " conjure a wall of fire and stops it, and had to quickly choose" ,weapon1," or " ,weapon2,'.');
+console.log("The wizard arrives at the ice witches hideout.");
+console.log("",character2, " conjures the biggest ice ball at him. The" ,character1, " conjure a wall of fire and stops it, and had to quickly choose" ,weapon1," or " ,weapon2,'.');
 console.log("",weapon1," is faster and gives the" ,character1, "more movement. ",weapon2," is heavy and drains half of his magic energy.");
-console.log("",character1, "choose ",weapon2,"for more powerful magic, " + "Then the" ,character1, "had to choose which witch to kill first");
+console.log("",character1, "choose ",weapon2,"for more powerful magic, " + "Then the" ,character1, "had to choose which witch to kill first.");
 
 
 // Function
@@ -63,7 +63,8 @@ var character2 = true
 if (character2 === true){
 
 console.log("Ice witch1 is the leader destroy her.");
-
+} else { 
+	console.log("find the leader");
 
 };
 
