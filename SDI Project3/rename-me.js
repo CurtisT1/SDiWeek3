a9a15b9bfb3408fdf8b4e8eeccb10312139db6d0
CurtisT1 <curtist2fullsail.edu>
2014-06-19 03:35:14 -0500

    Last

--- SDI Project3/rename-me.js
+++ SDI Project3/rename-me.js
@@ -10,8 +10,16 @@ var character3 = "Ice witch2";
 var character4 = "Ice witch3";
 var weapon1 = "Wand";
 var weapon2 = "Staff";
-var move1 = "Firestorm";
-var move2 = "Meteor showers";
+var move1 = "Meteor showers";
+
+
+// Outputs of 
+	
+	
+console.log("The, wizard arrives at the ice witches hideout");
+console.log("The" ,character2, " conjures the biggest ice ball at him. The" ,character1, " conjure a wall of fire and stops it, and had to quickly choose" ,weapon1," or " ,weapon2,'.');
+console.log("",weapon1," is faster and gives the" ,character1, "more movement. ",weapon2," is heavy and drains half of his magic energy.");
+console.log("",character1, "choose ",weapon2,"for more powerful magic, " + "Then the" ,character1, "had to choose which witch to kill first");
 
 
 // Function
@@ -25,10 +33,11 @@ var move2 = "Meteor showers";
 	useWeapon("wizard");
 	useWeapon("character2");
 	console.log(character1Name + " choose " + weapon1);
-	};
-	
-	
+	};	
+
+
 // Arrays 
+
 
 var characterNames = ["Ice witch1" , "Ice witch2" , "Ice witch3"];
 
@@ -46,7 +55,6 @@ console.log(characterNames[i] + "is the leader kill her and the others Ice witch
 };
 
 
-
 // Nested Conditional
 
 
@@ -60,17 +68,7 @@ console.log("Ice witch1 is the leader destroy her.");
 };
 
 
-
-
-
-
-
-
-
-	
-	
-	
-	//Loop 
+// While Loop 
 
 var move2 = 4;
 while(move2 > 0) {
